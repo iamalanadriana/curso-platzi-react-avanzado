2fdc2792f028de4390a2cbf58470387b2162c86f
iam alex <jhonmira_804@hotmail.com>
2021-08-18 20:55:09 -0500

    xbrand1.0

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-ReactDom.render(<h1>hola mundo...</h1>, document.getElementById('app'))
+ReactDom.render(<h1>hola mundo yes...</h1>, document.getElementById('app'))
